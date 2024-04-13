8b51c15327f982581d6be68b194ce80a9dca7e6d
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2024-04-13 02:24:42 -0300

    word position control now works. Working on visualization next

--- 1_13_3.c
+++ 1_13_3.c
@@ -2,21 +2,19 @@
 
 #define IN 1
 #define OUT 0
-#define MAX_LEN_WORD 45
+#define MAX_LEN_WORD 45 /*according to google, longest word in english is 45 letters long lol*/
 
-
-/* this program only takes notes of the positions themselves so far. Find a way to make it so that the counting restarts I think*/
 
 int main ()
 {
 
-    int c, i, char_count, word;
+    int c, i, j, char_count, word;
     int word_pos_count[MAX_LEN_WORD];
 
+    j = 0;
     char_count = 0;
     word = OUT;
 
-    /* assigns a value for every place in the array*/
     for (i = 0; i < MAX_LEN_WORD; ++i)
         word_pos_count[i] = 0;
 
@@ -29,6 +27,7 @@ int main ()
                 word = IN;
                 ++char_count;
             }
+        /*new word to be starting (state IN, but blank space identified)*/
         else if (word == IN)
             {
                 for (i = 0; i < char_count; ++i)
@@ -36,22 +35,45 @@ int main ()
 
                 word = OUT;
                 char_count = 0;
-            }
+            }   
         
     }
 
-
-    /* -1 because it's counting one more caracther for some god forsaken reason
+    /*exec for ending first/last word*/
     for (i = 0; i < char_count-1; ++i)
-            ++word_pos_count[i]; */
+        ++word_pos_count[i];
 
+    /* reusing char count to count length of lengthiest word*/
+    char_count = 0;
 
-    /* */
     for (i = 0; i < MAX_LEN_WORD; ++i)
-        printf ("%d\n", word_pos_count[i]);
+        {
+            if (word_pos_count[i] != 0)
+                ++char_count;
+        }
+        /*printf ("%d\n", word_pos_count[i]); - legacy thing that worked*/
+
+
+    /*creates biggest possible histogram array, considering lengthiest word*/
+    while (j != char_count)
+    {
+        for (i = 0; i < char_count; ++i)
+        {
+            /* find a way to make the array only print according to the the word pos count. This doesn't work currently*/
+            if (word_pos_count[i] < char_count)
+            {
+                putchar ('c');
+                putchar (' ');
+            }    
+        }
+
+        putchar ('\n');
+        ++j;
+    }
+
 
 }
 
 
-
+/* next up: finish the vertical line visualization*/
 
